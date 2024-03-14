08a5d7b80628dcbb811ad7d938fd4c0351adeefa
Oak Nelson <31452202+nelsonand@users.noreply.github.com>
2024-03-14 10:43:56 -0700

    build_lmode

---
+++ popsim/simulators/comet_mirror/build_lmode.py
@@ -0,0 +1,63 @@
+from cfspopcon.unit_handling import Quantity
+from popsim.algorithms.density import MultiSpeciesDensityModel
+from popsim.algorithms.geometry import GeometryCFSPopcon
+from popsim.enums import FuelSpecies
+from popsim.simulators.comet_mirror.model import CometMirror, Params, State
+from popsim.tests import load_sparc_q1l_data_f
+
+
+def build_lmode():
+    input_parameters, algorithm, points, impurity_types, impurity_concentrations = load_sparc_q1l_data_f()
+    for k, v in input_parameters.items():
+        if isinstance(v, Quantity):
+            input_parameters[k] = v.magnitude
+
+    model = CometMirror(
+        species=[FuelSpecies.Deuterium, FuelSpecies.Tritium, *impurity_types],
+        profile_form=input_parameters["profile_form"],
+        energy_confinement_scaling=input_parameters["energy_confinement_scaling"],
+    )
+
+    geom = GeometryCFSPopcon(
+        major_radius=input_parameters["major_radius"],
+        inverse_aspect_ratio=input_parameters["inverse_aspect_ratio"],
+        areal_elongation=input_parameters["areal_elongation"],
+        elongation_ratio_sep_to_areal=input_parameters["elongation_ratio_sep_to_areal"],
+        triangularity_psi95=input_parameters["triangularity_psi95"],
+        triangularity_ratio_sep_to_psi95=input_parameters["triangularity_ratio_sep_to_psi95"],
+    )
+
+    params = Params(
+        magnetic_field_on_axis=input_parameters["magnetic_field_on_axis"],
+        plasma_current=input_parameters["plasma_current"],
+        fraction_of_external_power_coupled=input_parameters["fraction_of_external_power_coupled"],
+        normalized_inverse_temp_scale_length=input_parameters["normalized_inverse_temp_scale_length"],
+        electron_density_peaking_offset=input_parameters["electron_density_peaking_offset"],
+        ion_density_peaking_offset=input_parameters["ion_density_peaking_offset"],
+        temperature_peaking=input_parameters["temperature_peaking"],
+        ion_to_electron_temp_ratio=input_parameters["ion_to_electron_temp_ratio"],
+        confinement_time_scalar=input_parameters["confinement_time_scalar"],
+        P_aux_MW=9.4,  # From CFSPOPON (could tweak)
+        geometry=geom,
+        fueling={
+            FuelSpecies.Deuterium: 1.0,
+            FuelSpecies.Tritium: 1.0,
+        },
+        particle_confinement_scalar={k: 7.0 for k in model.species},
+    )
+
+    average_ion_density = 13  # From CFSPOPON (could tweak)
+    density_states = {
+        FuelSpecies.Deuterium: average_ion_density / 2,
+        FuelSpecies.Tritium: average_ion_density / 2,
+    } | {imp: 0.0001 * average_ion_density for imp in impurity_types}
+
+    state = State(
+        stored_energy=8,  # From CFSPOPON (could tweak)
+        density_state=MultiSpeciesDensityModel.State(volume_average_ion_densities=density_states),
+    )
+    return model, state, params
+
+
+if __name__ == "__main__":
+    build_lmode()
--- popsim/tests/__init__.py
+++ popsim/tests/__init__.py
@@ -24,6 +24,23 @@ def load_sparc_prd_data_f():
 
     return input_parameters, algorithm, points, impurity_types, impurity_concentrations
 
+def load_sparc_q1l_data_f():
+    sparc_path = os.path.join(PACKAGE_ROOT, "../cfspopcon/example_cases/SPARC_Q1L")
+    input_parameters, algorithm, points = cfspopcon.read_case(sparc_path)
+    algorithm.validate_inputs(input_parameters)
+
+
+    impurity_types = [Impurity(impurity.value) for impurity in input_parameters["impurities"].dim_species.data]
+
+    impurity_concentrations = dict(
+        zip(
+            impurity_types,
+            input_parameters["impurities"].values,
+        )
+    )
+
+    return input_parameters, algorithm, points, impurity_types, impurity_concentrations
+
 @pytest.fixture
 def load_sparc_prd_data():
     return load_sparc_prd_data_f()
