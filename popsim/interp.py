0c3bd72298e8bd41825941db20257d2267b371cf
Allen Wang <allenw@mit.edu>
2024-03-29 17:55:24 -0400

    Add docstring

--- popsim/interp.py
+++ popsim/interp.py
@@ -8,16 +8,28 @@ from popsim.tree_util import tree_transpose
 
 
 def interp_time_dic(
-    dic_trees: dict[float, PyTree[Real]], kind: str = "linear"
-) -> typing.Union[diffrax.LinearInterpolation, diffrax.CubicInterpolation]:
+    dic_trees: dict[float, PyTree[Real]], interp_type: str = "linear"
+) -> typing.Union[PyTree[diffrax.LinearInterpolation], PyTree[diffrax.CubicInterpolation]]:
+    """Given a dictionary where keys are times and values are trees, interpolate the trees at the times.
+
+    Args:
+        dic_trees (dict[float, PyTree[Real]]): A dictionary where keys are times and values are trees.
+        interp_type (str, optional): interpolation interp_type. Can be "linear" or "cubic". Defaults to "linear".
+
+    Raises:
+        ValueError: if interp_type is not "linear" or "cubic".
+
+    Returns:
+        typing.Union[PyTree[diffrax.LinearInterpolation], PyTree[diffrax.CubicInterpolation]]: _description_
+    """
     times = jnp.array(list(dic_trees.keys()))
     trees = tree_transpose(list(dic_trees.values()))
-    if kind == "linear":
+    if interp_type == "linear":
         return diffrax.LinearInterpolation(ts=times, ys=trees)
-    elif kind == "cubic":
+    elif interp_type == "cubic":
         return diffrax.CubicInterpolation(ts=times, coeffs=diffrax.backward_hermite_coefficients(times, trees))
     else:
-        raise ValueError(f"Unknown kind: {kind}")
+        raise ValueError(f"Unknown interp_type: {interp_type}")
 
 
 def cubic_interp(ts, tree) -> diffrax.CubicInterpolation:
