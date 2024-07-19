b4f72ccbf50d7300b8928c594c9016dacd7d7899
ZanderKeith <zkeith@mit.edu>
2024-07-19 10:27:39 -0400

    Fleshed out test for stateless magnetic diagnostics module

--- popsim/tests/test_modules/test_magnetic_diagnostics.py
+++ popsim/tests/test_modules/test_magnetic_diagnostics.py
@@ -1,12 +1,12 @@
 import jax
-import os
+import jax.numpy as jnp
 
 import popsim.param_utils as param_utils
 from popsim.modules.tearing import Island, Tearing, DisruptionPhase, TearingPhase
-from popsim.simulate import simulate
 from popsim.modules.magnetic_diagnostics import LowNArray, load_lown_config
 
 def test_lown_array_same_amplitude_diff_phases():
+    """Ensure the Low-N Array module returns the same amplitude for different phases of the same mode."""
     jax.config.update("jax_platforms", "cpu")
 
     islands = [Island(2, 1), Island(3, 2)]
@@ -41,28 +41,71 @@ def test_lown_array_same_amplitude_diff_phases():
 
     lown_array_module = LowNArray(config=lown_array_config)
 
-    W = {island: 0.0 for island in islands}
-    F = {island: 0.0 for island in islands}
-    mode_phase={island: 0.0 for island in islands}
+    test_phases = jnp.linspace(0, 2*jnp.pi, 10)
 
-    tearing_state = Tearing.State(W=W, F=F, mode_phase=mode_phase)
+    test_tearing_states = [
+        Tearing.State(W={island: 50.0 for island in islands},
+                      F={island: 1e3 for island in islands},
+                      mode_phase={island: phase for island in islands})
+        for phase in test_phases
+    ]
 
-    # Get the measured output given this state
+    resulting_magnitudes = []
+    for tearing_state in test_tearing_states:
+        _, tearing_output = tearing_module(tearing_state, tearing_params)
+        lown_array_out = lown_array_module(tearing_output, tearing_module.config.islands)
+        reconstructed_magnitudes = lown_array_out.reconstructed_magnitudes
+        resulting_magnitudes.append(reconstructed_magnitudes)
+
+    for island in islands:
+        island_magnitudes = jnp.asarray([all_magnitudes[island.n] for all_magnitudes in resulting_magnitudes])
+        assert jnp.allclose(island_magnitudes, jnp.ones_like(island_magnitudes) * island_magnitudes[0])
+
+def test_lown_array_nonexistant_mode():
+    """Ensure the measured amplitude of a nonexistant mode is very small."""
+
+    jax.config.update("jax_platforms", "cpu")
+
+    islands = [Island(3, 2)]
+
+    dt = 1e-4 / 3  # s
+    time_base = param_utils.make_time_base(t0=0.0, t1=3.0, dt=dt)
+    config = Tearing.Config(
+        magx_time=time_base,
+        islands = islands
+    )
+
+    rot_dur = 1.0
+    locking_dur = 0.2
+    
+    tearing_params = Tearing.Params(
+        rot_dur=rot_dur,  # s
+        locking_dur=locking_dur,  # s
+        disruption_phase=DisruptionPhase.NONE,
+        tearing_phase=TearingPhase.ROTATING
+    )
+
+    tearing_module = Tearing(config=config)
+
+    # Create a LowNArray module
+    probe_connections, func_Bp_per_A = load_lown_config("popsim/data/tearing/")
+
+    lown_array_config = LowNArray.Config(
+        func_Bp_per_A=func_Bp_per_A,
+        probe_connections=probe_connections,
+        reconstructed_modes=[1,2,3]
+    )
+
+    lown_array_module = LowNArray(config=lown_array_config)
+
+    tearing_state = Tearing.State(W={island: 50.0 for island in islands},
+                      F={island: 1e3 for island in islands},
+                      mode_phase={island: 0.0 for island in islands})
+    
+
     _, tearing_output = tearing_module(tearing_state, tearing_params)
     lown_array_out = lown_array_module(tearing_output, tearing_module.config.islands)
     reconstructed_magnitudes = lown_array_out.reconstructed_magnitudes
 
-    # Reconstructed magnitudes should be close to 0 for all islands, since that is what the state is
-    for island in islands:
-        assert reconstructed_magnitudes[island.n] < 1e-6
-
-    # Update the mode phases to be something else
-    mode_phase={island: 300.0 for island in islands}
-    tearing_state = Tearing.State(W=W, F=F, mode_phase=mode_phase)
-
-    # Get the measured output from a given Tearing module output
-    _, tearing_output = tearing_module(tearing_state, tearing_params)
-
-
-    # Get the Tearing module's output
-    print("beans")
+    assert reconstructed_magnitudes[1] < 1e-6*reconstructed_magnitudes[2]
+    assert reconstructed_magnitudes[3] < 1e-6*reconstructed_magnitudes[2]
