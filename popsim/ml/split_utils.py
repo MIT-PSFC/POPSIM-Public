e9b06c87322aa7cfb8e93a498e43546b432fbd00
Allen Wang <allenw@mit.edu>
2024-07-22 16:27:46 -0400

    Make popsim.ml module and add split_utils

---
+++ popsim/ml/split_utils.py
@@ -0,0 +1,87 @@
+import math
+from collections.abc import Sequence
+from itertools import accumulate
+from typing import Union
+
+import jax
+import xarray as xr
+
+
+def fracs_to_lengths(n_data: int, fracs: Sequence[float]) -> Sequence[int]:
+    """Convert a list of fractions to a list of lengths.
+
+    Args:
+        n_data (int): number of data points.
+        fracs (Sequence[float]): sequence of fractions.
+
+    Returns:
+        Sequence[int]: sequence of lengths that should add up to n_data.
+    """
+    if not isinstance(n_data, int) or n_data <= 0:
+        raise ValueError("n_data must be a positive integer.")
+
+    if round(sum(fracs), 2) != 1.0:
+        raise ValueError("Sum of fractions in fracs must add up to 1.0.")
+
+    # Calculate segment lengths and round them
+    segment_lengths = [round(frac * n_data) for frac in fracs]
+
+    # Adjust the segment lengths to ensure they sum up to n_data
+    while sum(segment_lengths) != n_data:
+        if sum(segment_lengths) > n_data:
+            segment_lengths[segment_lengths.index(max(segment_lengths))] -= 1
+        else:
+            segment_lengths[segment_lengths.index(min(segment_lengths))] += 1
+    if not sum(segment_lengths) == n_data:
+        raise ValueError("Sum of segment lengths does not equal n_data. This is a bug, please report it.")
+    return segment_lengths
+
+
+def random_split(
+    n_data: int,
+    lengths_or_fracs: Sequence[Union[int, float]],
+    key: jax.random.PRNGKey,
+) -> list[jax.Array]:
+    """Generate indicies to split a dataset into non-overlapping new datasets.
+
+    Args:
+        n_data (int): size of the dataset to be split.
+        lengths_or_fracs (Sequence[Union[int, float]]): lengths or fractions of splits to be produced.
+        key (jax.random.PRNGKey): key to use for random number generation.
+
+    Returns:
+        List[jax.Array]: list of indicies to split the dataset.
+    """
+    if math.isclose(sum(lengths_or_fracs), 1) and sum(lengths_or_fracs) <= 1:
+        lengths = fracs_to_lengths(n_data, lengths_or_fracs)
+    else:
+        if sum(lengths_or_fracs) != n_data:
+            raise ValueError("Sum of input lengths does not equal n_data!")
+        lengths = lengths_or_fracs
+
+    # Cannot verify that dataset is Sized
+    if sum(lengths) != n_data:
+        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
+
+    indices = jax.random.permutation(
+        key,
+        n_data,
+    )
+    return [indices[offset - length : offset] for offset, length in zip(accumulate(lengths), lengths)]
+
+
+def split_dataset_by_coords(ds: xr.Dataset, fracs: Sequence[float], coord: str, key: jax.random.PRNGKey) -> Sequence[xr.Dataset]:
+    """Split a dataset into non-overlapping new datasets based on a coordinate.
+
+    Args:
+        ds (xr.Dataset): dataset to be split.
+        fracs (Sequence[float]): fractions of splits to be produced.
+        coord (str): coordinate to split the dataset by.
+        key (jax.random.PRNGKey): key to use for random number generation.
+
+    Returns:
+        Sequence[xr.Dataset]: sequence of datasets.
+    """
+    n_data = len(ds[coord])
+    lengths = fracs_to_lengths(n_data, fracs)
+    return [ds.isel({coord: idxs}) for idxs in random_split(n_data, lengths, key)]
---
+++ popsim/tests/test_ml/test_split_utils.py
@@ -0,0 +1,61 @@
+import math
+import random
+
+import jax
+import jax.numpy as jnp
+
+from popsim.ml.split_utils import fracs_to_lengths, random_split
+
+
+def test_fracs_to_lengths():
+    def generate_three_numbers_sum_to_one():
+        # Generate two random numbers between 0 and 1
+        first, second = random.random(), random.random()
+
+        # Ensure they are in ascending order
+        first, second = sorted([first, second])
+
+        # Calculate the three segments
+        a = first
+        b = second - first
+        c = 1 - second
+
+        return a, b, c
+
+    n_fracs_try = 100
+    lengths_to_try = [2**i for i in range(32)]
+    for _ in range(n_fracs_try):
+        fracs = generate_three_numbers_sum_to_one()
+        assert math.isclose(sum(fracs), 1.0)
+
+        for total_length in lengths_to_try:
+            lengths = fracs_to_lengths(total_length, fracs)
+            assert sum(lengths) == total_length
+            assert len(lengths) == 3
+            assert all(isinstance(le, int) for le in lengths)
+            assert all(
+                abs(length - round(total_length * frac)) <= 1
+                for length, frac in zip(lengths, fracs)
+            )
+
+
+def test_random_split():
+    n_data = 10
+    fracs = [0.7, 0.2, 0.1]
+    key = jax.random.PRNGKey(42)
+    idx_sets = random_split(n_data, fracs, key)
+    for frac, idx_set in zip(fracs, idx_sets):
+        assert round(frac * n_data) - len(idx_set) <= 1
+
+    concatenated_idxs = jnp.concatenate(idx_sets)
+    assert len(concatenated_idxs) == n_data
+    assert jnp.all(jnp.sort(concatenated_idxs) == jnp.arange(n_data))
+    assert len(idx_sets[0]) == 7
+    assert len(idx_sets[1]) == 2
+    assert len(idx_sets[2]) == 1
+
+    lengths = [7, 2, 1]
+    idx_sets = random_split(n_data, lengths, key)
+    assert len(idx_sets[0]) == lengths[0]
+    assert len(idx_sets[1]) == lengths[1]
+    assert len(idx_sets[2]) == lengths[2]
