c82b19c47f697eb212e1b3741f55699133c7719a
Allen Wang <allenw@mit.edu>
2024-08-02 12:15:40 -0400

    Add a dt to test_tearing to avoid off by one errors

--- popsim/tests/test_modules/test_tearing.py
+++ popsim/tests/test_modules/test_tearing.py
@@ -66,7 +66,7 @@ def test_mode_growth_and_freq(tearing_test_sim):
 
         current = sol_xarray[f"aux.mode_current.{str(mode)}"]
         assert current.sel(time=0, method="nearest") == 0.0
-        assert current.sel(time=aux_data["trigger_time"], method="nearest") > 0.0
+        assert current.sel(time=aux_data["trigger_time"] + aux_data["dt"], method="nearest") > 0.0
         assert current.sel(time=aux_data["cq_time"]+1e3*aux_data["dt"], method="nearest") == 0.0
 
         F_dot = sol_xarray[f"aux.state_dot.F.{str(mode)}"]
@@ -76,5 +76,5 @@ def test_mode_growth_and_freq(tearing_test_sim):
 
         F = sol_xarray[f"aux.mode_freq.{str(mode)}"]
         assert F.sel(time=0, method="nearest") == 0.0
-        assert F.sel(time=aux_data["trigger_time"], method="nearest") == INITIAL_ROT_FREQ[mode]
+        assert F.sel(time=aux_data["trigger_time"]+aux_data["dt"], method="nearest") == INITIAL_ROT_FREQ[mode]
         assert F.sel(time=aux_data["lock_time"], method="nearest") == 0.0
