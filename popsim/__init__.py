32491d352c15b1e25a23ec2e4964cf08e189c3c2
Allen Wang <allenw@mit.edu>
2024-08-07 11:36:14 -0400

    Separate out cmod_test_dataset and load_cmod_test_dataset

--- popsim/__init__.py
+++ popsim/__init__.py
@@ -11,5 +11,6 @@ config.update("jax_enable_x64", True)
 PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
 SUBMODULES_DIR = os.path.join(PACKAGE_ROOT, "../submodules")
 TORAX_QLKNN_MODEL_PATH = os.path.join(SUBMODULES_DIR, "qlknn-hyper")
+DATA_DIR = os.path.join(PACKAGE_ROOT, "data")
 
 __all__ = ["ModuleBase", "discrete_time_field"]
--- popsim/tests/test_ml/fixtures.py
+++ popsim/tests/test_ml/fixtures.py
@@ -1,10 +1,15 @@
 import xarray as xr
 import os
 import pytest
+import popsim
+
+
+def load_cmod_test_dataset():
+    # TODO(allenw): replace with path to commited data once permissions are sorted out.
+    path_to_data = os.path.join(popsim.DATA_DIR, "cmod/saperstein_july_2024_subset.nc")
+    ds = xr.open_dataset(path_to_data)
+    return ds
 
 @pytest.fixture
 def cmod_test_dataset():
-    # TODO(allenw): replace with path to commited data once permissions are sorted out.
-    path_to_data = os.path.join(os.environ["ML_DATA_DUMP"], "CMOD/disruptions_warning_database.nc")
-    ds = xr.open_dataset(path_to_data)
-    return ds
+    return load_cmod_test_dataset()
