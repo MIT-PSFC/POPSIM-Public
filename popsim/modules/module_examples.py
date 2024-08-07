518a019c0ee432b95e495106fbfd44e82052d77c
Allen Wang <allenw@mit.edu>
2024-08-07 17:40:55 -0400

    Get rid of discrete_example module and notebook. Introduce module_examples and intro_to_modules.ipynb

---
+++ popsim/modules/module_examples.py
@@ -0,0 +1,173 @@
+from enum import IntEnum
+
+import chex
+import jax.numpy as jnp
+
+from popsim import ModuleBase, discrete_time_field
+from popsim.logic_utils import select_w_tuples
+
+"""
+Contains three example modules for automated testing and documentation purposes:
+    1. BasicLorenz: A minimum example module that implements the Lorenz equations and introduces the four categories of variables.
+
+"""
+
+
+@chex.dataclass
+class BasicLorenz(ModuleBase):
+    @chex.dataclass
+    class Config:
+        # Define the configuration variables for the module.
+        xlims: tuple[float, float] = (-20.0, 20.0)  # Lower and upper limits for x
+        ylims: tuple[float, float] = (-20.0, 20.0)  # Lower and upper limits for y
+        zlims: tuple[float, float] = (-20.0, 20.0)  # Lower and upper limits for z
+
+    @chex.dataclass
+    class State:
+        # Define the state variables for the module.
+        x: float
+        y: float
+        z: float
+
+    @chex.dataclass
+    class Output:
+        # Define the output variables that will be returned by the module.
+        distance_from_origin: float  # L2 norm of the state
+
+    @chex.dataclass
+    class Params:
+        # Define the (possibly time-dependent) parameters for the module.
+        rho: float
+        sigma: float
+        beta: float
+
+    config: Config
+
+    def __init__(self, config):
+        self.config = config
+
+    def __call__(self, state: State, params: Params) -> tuple[State, Output]:
+        def val_in_limits(val, lims):
+            # Function to check a single variable is within the limits
+            return jnp.logical_and(val > lims[0], val < lims[1])
+
+        # Check if all variables are within the limits.
+        in_limits = jnp.all(
+            jnp.array(
+                [
+                    val_in_limits(state.x, self.config.xlims),
+                    val_in_limits(state.y, self.config.ylims),
+                    val_in_limits(state.z, self.config.zlims),
+                ]
+            )
+        )
+
+        # Lorenz equations.
+        x_dot = params.rho * (state.y - state.x)
+        y_dot = state.x * (params.sigma - state.z) - state.y
+        z_dot = state.x * state.y - params.beta * state.z
+
+        # If the state is out of limits, set the derivatives to zero.
+        state_dot = BasicLorenz.State(
+            x=jnp.where(in_limits, x_dot, 0.0),
+            y=jnp.where(in_limits, y_dot, 0.0),
+            z=jnp.where(in_limits, z_dot, 0.0),
+        )
+
+        # Make an output.
+        out = BasicLorenz.Output(distance_from_origin=jnp.sqrt(state.x**2 + state.y**2 + state.z**2))
+        return state_dot, out
+
+
+class ExampleDisruptedState(IntEnum):
+    NOT_DISRUPTED = 0
+    DISRUPTED = 1
+
+
+@chex.dataclass
+class DiscreteTimeExample(ModuleBase):
+    @chex.dataclass
+    class Config:
+        disruptivity_threshold: float
+
+    @chex.dataclass
+    class State:
+        disrupted_state: ExampleDisruptedState = discrete_time_field(default=ExampleDisruptedState.NOT_DISRUPTED)
+
+    @chex.dataclass
+    class Params:
+        disruptivity: float
+
+    @chex.dataclass
+    class Output:
+        pass
+
+    config: Config
+
+    def __init__(self, config):
+        self.config = config
+
+    def __call__(
+        self, state: "DiscreteTimeExample.State", params: "DiscreteTimeExample.Params"
+    ) -> tuple["DiscreteTimeExample.State", "DiscreteTimeExample.Output"]:
+        # If the disruptivity is above the threshold, set the state to DISRUPTED.
+        # If the state is already DISRUPTED, keep it that way, regardless of the disruptivity.
+        disruptivity_above_threshold = params.disruptivity > self.config.disruptivity_threshold
+        next_state = jnp.where(
+            jnp.logical_or(disruptivity_above_threshold, state.disrupted_state == ExampleDisruptedState.DISRUPTED),
+            ExampleDisruptedState.DISRUPTED,
+            ExampleDisruptedState.NOT_DISRUPTED,
+        )
+
+        # NOTE: this is not state_dot anymore, but the state at the next time step.
+        state_out = DiscreteTimeExample.State(disrupted_state=next_state)
+        out = DiscreteTimeExample.Output()
+        return state_out, out
+
+
+@chex.dataclass
+class HybridExample(ModuleBase):
+    @chex.dataclass
+    class Config:
+        pass
+
+    @chex.dataclass
+    class State:
+        y: float  # Continuous-time state variable for the position.
+        sign: int = discrete_time_field()  # Discrete-time state variable for the sign of the time derivative.
+
+    @chex.dataclass
+    class Params:
+        speed: float
+        ylims: tuple[float, float]
+
+    @chex.dataclass
+    class Output:
+        pass
+
+    def __call__(
+        self, state: "HybridExample.State", params: "HybridExample.Params"
+    ) -> tuple["HybridExample.State", "HybridExample.Output"]:
+        # If the state is at the upper limit, set the sign to -1.
+        # If the state is at the lower limit, set the sign to 1.
+        # Otherwise, keep the sign the same.
+        at_upper_lim = state.y >= params.ylims[1]
+        at_lower_lim = state.y <= params.ylims[0]
+
+        conditions_and_choices = [
+            (at_upper_lim, -1),
+            (at_lower_lim, 1),
+            (jnp.logical_and(~at_upper_lim, ~at_lower_lim), state.sign),
+        ]
+        next_sign = select_w_tuples(conditions_and_choices, default=1)
+
+        # NOTE: the output is no longer purely a "state_dot" or the state at the next time step, but rather a mix.
+        # The continuous part of the state is still a time derivative, but the discrete part is the state value at
+        # the next time step of the simulation.
+        ydot = params.speed * state.sign
+        state_out = HybridExample.State(
+            y=ydot,
+            sign=next_sign,
+        )
+        out = HybridExample.Output()
+        return state_out, out
--- popsim/tests/test_simulate.py
+++ popsim/tests/test_simulate.py
@@ -1,7 +1,9 @@
 import popsim.simulate as simulate
 import pytest
 import chex
-from popsim import ModuleBase, discrete_time_field
+from popsim import ModuleBase
+from popsim.modules.module_examples import DiscreteTimeExample, HybridExample, ExampleDisruptedState
+from popsim.param_utils import make_time_base
 import jax.numpy as jnp
 from popsim.xarray_utils import time_and_pytree_to_xarray, solution_to_xarray
 
@@ -40,38 +42,21 @@ def pure_continuous_time_module():
     return ContinuousTimeModule, initial_state
 
 @pytest.fixture
+def pure_discrete_time_module():
+    module = DiscreteTimeExample(config=DiscreteTimeExample.Config(disruptivity_threshold=0.9))
+    time_base = make_time_base(0.0, 1.0, 1e-3)
+    initial_state = DiscreteTimeExample.State(disrupted_state=ExampleDisruptedState.NOT_DISRUPTED)
+    params = DiscreteTimeExample.Params(disruptivity={0.0: 0.1, 0.5: 1.0, 0.75: 0.0})  # Time-dependent disruptivity.
+    return module, time_base, initial_state, params
+
+@pytest.fixture
 def hybrid_time_module():
-    @chex.dataclass
-    class HybridTimeModule(ModuleBase):
-        @chex.dataclass
-        class Config:
-            pass
-
-        @chex.dataclass
-        class State:
-            x: int = discrete_time_field()
-            y: float
-
-        @chex.dataclass
-        class Output:
-            z: float
-
-        @chex.dataclass
-        class Params:
-            a: float = 0
-
-        config: Config
-
-        def __init__(self, config):
-            self.config = config
-
-        def __call__(self, state: State, params: Params) -> tuple[State, Output]:
-            state_dot = HybridTimeModule.State(x=state.x + 1, y=-state.y)
-            out = HybridTimeModule.Output(z=jnp.abs(state.y))
-            return state_dot, out
-    
-    initial_state = HybridTimeModule.State(x=0, y=-1.0)
-    return HybridTimeModule, initial_state
+    module = HybridExample()
+    time_base = make_time_base(0.0, 10.0, 1e-3)
+    initial_state = HybridExample.State(y=0.0, sign=1)
+    final_lim_mag = 0.01
+    params = HybridExample.Params(speed=1.0, ylims=({0.0: -1.0, 10.0: -final_lim_mag}, {0.0: 1.0, 10.0: final_lim_mag}))
+    return module, time_base, initial_state, params
 
 @pytest.mark.parametrize("return_xarray", [True, False])
 @pytest.mark.parametrize("stepper_type", list(simulate.StepperType))
@@ -109,46 +94,65 @@ def test_simulate_continuous_module(pure_continuous_time_module, return_xarray, 
     if multi_sim:
         assert "simulation" in sol.dims
 
+
 @pytest.mark.parametrize("return_xarray", [True, False])
 @pytest.mark.parametrize("stepper_type", list(simulate.StepperType))
 @pytest.mark.parametrize("multi_sim", [True, False])
-def test_simulate_hybrid_module(hybrid_time_module, return_xarray, stepper_type, multi_sim):
-    HybridTimeModule, initial_state = hybrid_time_module
-
-    module = HybridTimeModule(config=HybridTimeModule.Config())
-
+def test_simulate_discrete(pure_discrete_time_module, return_xarray, stepper_type, multi_sim):
+    module, time_base, initial_state, params = pure_discrete_time_module
     if multi_sim:
-        params = [HybridTimeModule.Params(a=0.0), HybridTimeModule.Params(a=1.0)]
-    else:
-        params = HybridTimeModule.Params()
-    ts = jnp.linspace(0.0, 10.0, 100)
+        params = [params, params]
 
     # Expect an error if the stepper type is Diffrax.
     if stepper_type == simulate.StepperType.DIFFRAX:
         with pytest.raises(ValueError):
-            sol = simulate.simulate(module, ts, initial_state, params, stepper_type=stepper_type, return_xarray=return_xarray)
+            sol = simulate.simulate(module, time_base, initial_state, params, stepper_type=stepper_type, return_xarray=return_xarray)
         return
     else:
-        sol = simulate.simulate(module, ts, initial_state, params, stepper_type=stepper_type, return_xarray=return_xarray)
+        sol = simulate.simulate(module, time_base, initial_state, params, stepper_type=stepper_type, return_xarray=return_xarray)
 
     # In the cases where we don't return an xarray, convert the solution to an xarray for comparison.
     if return_xarray == False:
         if stepper_type == simulate.StepperType.SIMPLE_EULER:
-            sol = time_and_pytree_to_xarray(ts, sol, multi_simulation=multi_sim)
+            sol = time_and_pytree_to_xarray(time_base, sol, multi_simulation=multi_sim)
+        elif stepper_type == simulate.StepperType.DIFFRAX:
+            sol = solution_to_xarray(sol, multi_simulation=multi_sim)
+        else:
+            raise ValueError("Stepper type not recognized.")
+        
+    # Check not disrupted at the start.
+    assert (sol['state.disrupted_state'].sel(time=slice(0.0, 0.4)) == ExampleDisruptedState.NOT_DISRUPTED).all()
+
+    # Check disrupted for the latter half.
+    assert (sol['state.disrupted_state'].sel(time=slice(0.5, 1.0)) == ExampleDisruptedState.DISRUPTED).all()
+
+@pytest.mark.parametrize("return_xarray", [True, False])
+@pytest.mark.parametrize("stepper_type", list(simulate.StepperType))
+@pytest.mark.parametrize("multi_sim", [True, False])
+def test_simulate_hybrid_module(hybrid_time_module, return_xarray, stepper_type, multi_sim):
+    module, time_base, initial_state, params = hybrid_time_module
+    if multi_sim:
+        params = [params, params]
+
+    # Expect an error if the stepper type is Diffrax.
+    if stepper_type == simulate.StepperType.DIFFRAX:
+        with pytest.raises(ValueError):
+            sol = simulate.simulate(module, time_base, initial_state, params, stepper_type=stepper_type, return_xarray=return_xarray)
+        return
+    else:
+        sol = simulate.simulate(module, time_base, initial_state, params, stepper_type=stepper_type, return_xarray=return_xarray)
+
+    # In the cases where we don't return an xarray, convert the solution to an xarray for comparison.
+    if return_xarray == False:
+        if stepper_type == simulate.StepperType.SIMPLE_EULER:
+            sol = time_and_pytree_to_xarray(time_base, sol, multi_simulation=multi_sim)
         elif stepper_type == simulate.StepperType.DIFFRAX:
             sol = solution_to_xarray(sol, multi_simulation=multi_sim)
         else:
             raise ValueError("Stepper type not recognized.")
     
-    # After 10 seconds, expect a significant amount of exponential decay of the state.
-    assert (jnp.abs(sol["state.y"].isel(time=-1).values) < 1.1 * jnp.exp(-jnp.max(ts)) * jnp.abs(initial_state.y)).all()
-
-    # Expect that z is the absolute value of y.
-    assert jnp.allclose(sol["output.z"].values, jnp.abs(sol["state.y"].values))
-
-    # Expect x to be incremented by 1 at each time step.
-    assert jnp.allclose(sol["state.x"].values, jnp.arange(0, len(ts)))
-
+    pad = 0.01
+    assert (sol["state.y"] <= sol["params.ylims.1"] + pad).all()
 
     # If multi_sim, check that there is a simulation dimension.
     if multi_sim:
