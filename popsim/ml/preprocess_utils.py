0a44a7b30e27fbfe9b7f944aea856ea06f8cdd38
Allen Wang <allenw@mit.edu>
2024-08-08 15:19:59 -0400

    Introduce preprocess_utils.py

---
+++ popsim/ml/preprocess_utils.py
@@ -0,0 +1,83 @@
+import typing
+
+import numpy as np
+import xarray as xr
+
+
+def mask_to_largest_group_mask(mask: xr.DataArray, episode_dim: str, time_dim: str) -> xr.DataArray:
+    """Given a boolean mask, generate a new mask that only includes the largest
+    group of contiguous True/1 values for each shot. In the case of a tie, the first group is selected.
+
+    Args:
+        mask (xr.DataArray): boolean mask to be processed. Int arrays of 0 and 1 are also accepted.
+        episode_dim (str): name of the episode dimension (e.g. "shot").
+        time_dim (str): name of the time dimension (e.g. "time").
+
+    Returns:
+        xr.DataArray: mask with only the largest group of contiguous True/1 values for each shot.
+    """
+
+    def _mask_to_largest_group_mask(mask: xr.DataArray) -> xr.DataArray:
+        # Check that the mask is int or bool. If it is bool, convert to int.
+        if mask.dtype == "bool":
+            mask = mask.astype(int)
+        elif mask.dtype == "int":
+            assert np.all(np.logical_or(mask == 0, mask == 1))
+        else:
+            raise ValueError(f"Expected mask to be of type int or bool, but got {mask.dtype}")
+
+        # Remove extraneous dimensions.
+        mask = mask.squeeze()
+
+        # If the mask is all False, there are no groups to consider.
+        if (mask == 0).all():
+            return mask
+
+        # At every point where the mask changes, increment a cumulative sum.
+        # This results in a unique number for each continguous group of Trues and Falses.
+        cumulative_number_of_mask_flips = (mask != mask.shift({time_dim: 1})).cumsum()
+
+        # Label all points where the mask is True with -1. Once that is done, what is left
+        # are the group numbers for each contiguous group of True values.
+        groups = cumulative_number_of_mask_flips.where(mask, other=-1)
+
+        # Compute the sizes of the groups and find the largest group.
+        group_sizes = groups.to_pandas().value_counts().drop(-1, errors="ignore")
+        largest_group = group_sizes.idxmax()
+        return groups == largest_group
+
+    return mask.groupby(episode_dim).map(lambda shot_mask: _mask_to_largest_group_mask(shot_mask))
+
+
+def shift_time_to_not_nan(
+    ds: xr.Dataset, episode_dim: str, time_dim: str, how: str = "any", subset: typing.Optional[typing.Iterable[typing.Hashable]] = None
+) -> xr.Dataset:
+    """For each shot in a dataset, shift the time dimension so that the first time slice is not NaN. Each shot is end-padded with NaNs. To specify that only a subset of the variables should be considered when determining the first non-NaN time slice, pass a list of variable names to the `subset` argument.
+
+    Args:
+        ds (xr.Dataset): dataset to be processed.
+        episode_dim (str): name of the episode dimension (e.g. "shot").
+        time_dim (str): name of the time dimension (e.g. "time").
+        how (str, optional): Either "any" or "ally". Forwarded to xr.Dataset.dropna . Defaults to "any".
+        subset (typing.Optional[typing.Iterable[typing.Hashable]], optional): Forwarded to xr.Dataset.dropna . Defaults to None.
+
+    Returns:
+        xr.Dataset: dataset with the time dimension shifted so that the first time slice is not NaN.
+    """
+
+    def dropna_preserve_size(group):
+        # Drop NaNs across the 'time_slice' dimension.
+        cleaned_group = group.dropna(time_dim, how=how, subset=subset)
+
+        # If there are no time slices left, just return the cleaned group which is empty.
+        if cleaned_group[time_dim].size == 0:
+            return cleaned_group
+
+        # Find the first time slice remaining in the cleaned group.
+        # Find its index in the original group and shift the time dimension by that amount.
+        time_of_first_cleaned_slice = cleaned_group[time_dim].values[0]
+        time_index = np.where(group[time_dim].values == time_of_first_cleaned_slice)[0][0]
+        n_shift = -time_index
+        return group.shift({time_dim: n_shift})
+
+    return ds.groupby(episode_dim).map(dropna_preserve_size)
---
+++ popsim/tests/test_ml/test_preprocess_utils.py
@@ -0,0 +1,226 @@
+import pytest
+import xarray as xr
+import numpy as np
+
+from popsim.ml.preprocess_utils import mask_to_largest_group_mask, shift_time_to_not_nan
+
+@pytest.mark.parametrize("mask, expected", [
+    # Single Episode, Single Group
+    (
+        xr.DataArray([[0, 1, 1, 0, 1]], dims=["episode", "time"]),
+        xr.DataArray([[0, 1, 1, 0, 0]], dims=["episode", "time"])
+    ),
+    # Single Episode, Multiple Groups
+    (
+        xr.DataArray([[1, 1, 0, 1, 1, 1, 0, 1]], dims=["episode", "time"]),
+        xr.DataArray([[0, 0, 0, 1, 1, 1, 0, 0]], dims=["episode", "time"])
+    ),
+    # Multiple Episodes
+    (
+        xr.DataArray([[1, 1, 0, 1], [0, 1, 1, 1]], dims=["episode", "time"]),
+        xr.DataArray([[1, 1, 0, 0], [0, 1, 1, 1]], dims=["episode", "time"])
+    ),
+    # All False
+    (
+        xr.DataArray([[0, 0, 0, 0]], dims=["episode", "time"]),
+        xr.DataArray([[0, 0, 0, 0]], dims=["episode", "time"])
+    ),
+    # All True
+    (
+        xr.DataArray([[1, 1, 1, 1]], dims=["episode", "time"]),
+        xr.DataArray([[1, 1, 1, 1]], dims=["episode", "time"])
+    ),
+    # Non-Contiguous Dimension
+    (
+        xr.DataArray([[1, 0, 1, 0, 1]], dims=["episode", "time"]),
+        xr.DataArray([[1, 0, 0, 0, 0]], dims=["episode", "time"])
+    )
+])
+def test_mask_to_largest_group_mask(mask, expected):
+    result = mask_to_largest_group_mask(mask, episode_dim="episode", time_dim="time")
+    xr.testing.assert_equal(result, expected)
+
+def test_mask_to_largest_group_mask_invalid_dtype():
+    mask = xr.DataArray(
+        [[0.5, 1.0, 0.0, 1.0]],
+        dims=["episode", "time"]
+    )
+    with pytest.raises(ValueError, match="Expected mask to be of type int or bool"):
+        mask_to_largest_group_mask(mask, episode_dim="episode", time_dim="time")
+
+@pytest.mark.parametrize("ds, how, subset, expected", [
+    # how=Any: No NaN
+    (
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[1, 2, 3], [4, 5, 6]]),
+                "var2": (("shot", "time"), [[7, 8, 9], [10, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        ),
+        "any",
+        None,
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[1, 2, 3], [4, 5, 6]]),
+                "var2": (("shot", "time"), [[7, 8, 9], [10, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        )
+    ),
+    # how=Any: NaNs at Start
+    (
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, 1, 2], [np.nan, np.nan, 6]]),
+                "var2": (("shot", "time"), [[np.nan, 8, 9], [10, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        ),
+        "any",
+        None,
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[1, 2, np.nan], [6, np.nan, np.nan]]),
+                "var2": (("shot", "time"), [[8, 9, np.nan], [12, np.nan, np.nan]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        )
+    ),
+    # how=Any: NaNs at Start and Middle
+    (
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, np.nan, 2], [np.nan, 5, np.nan]]),
+                "var2": (("shot", "time"), [[np.nan, 8, 9], [np.nan, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        ),
+        "any",
+        None,
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[2, np.nan, np.nan], [5, np.nan, np.nan]]),
+                "var2": (("shot", "time"), [[9, np.nan, np.nan], [11, 12, np.nan]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        )
+    ),
+    # how=Any: Only use a subset of variables for determining the first non-NaN time slice
+    (
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, 1, 2], [4, np.nan, 6]]),
+                "var2": (("shot", "time"), [[np.nan, 8, 9], [np.nan, np.nan, np.nan]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        ),
+        "any",
+        ["var1"],
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[1, 2, np.nan], [4, np.nan, 6]]),
+                "var2": (("shot", "time"), [[8, 9, np.nan], [np.nan, np.nan, np.nan]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        )
+    ),
+    # how=Any: All NaNs in a Shot
+    (
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, np.nan, np.nan], [4, 5, 6]]),
+                "var2": (("shot", "time"), [[np.nan, np.nan, np.nan], [10, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        ),
+        "any",
+        None,
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, np.nan, np.nan], [4, 5, 6]]),
+                "var2": (("shot", "time"), [[np.nan, np.nan, np.nan], [10, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        )
+    ),
+    # how=All: NaNs at Start
+    (
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, np.nan, 2], [np.nan, np.nan, 6]]),
+                "var2": (("shot", "time"), [[np.nan, np.nan, 9], [10, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        ),
+        "all",
+        None,
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[2, np.nan, np.nan], [np.nan, np.nan, 6]]),
+                "var2": (("shot", "time"), [[9, np.nan, np.nan], [10, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        )
+    ),
+    # how=All: NaNs at Start and Middle
+    (
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, np.nan, np.nan], [np.nan, 5, np.nan]]),
+                "var2": (("shot", "time"), [[np.nan, np.nan, 9], [np.nan, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        ),
+        "all",
+        None,
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, np.nan, np.nan], [5, np.nan, np.nan]]),
+                "var2": (("shot", "time"), [[9, np.nan, np.nan], [11, 12, np.nan]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        )
+    ),
+    # how=All: Only use a subset of variables for determining the first non-NaN time slice
+    (
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, 1, 2], [4, np.nan, 6]]),
+                "var2": (("shot", "time"), [[np.nan, 8, 9], [np.nan, np.nan, np.nan]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        ),
+        "all",
+        ["var1"],
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[1, 2, np.nan], [4, np.nan, 6]]),
+                "var2": (("shot", "time"), [[8, 9, np.nan], [np.nan, np.nan, np.nan]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        )
+    ),
+    # how=All: All NaNs in a Shot
+    (
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, np.nan, np.nan], [4, 5, 6]]),
+                "var2": (("shot", "time"), [[np.nan, np.nan, np.nan], [10, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        ),
+        "all",
+        None,
+        xr.Dataset(
+            {
+                "var1": (("shot", "time"), [[np.nan, np.nan, np.nan], [4, 5, 6]]),
+                "var2": (("shot", "time"), [[np.nan, np.nan, np.nan], [10, 11, 12]])
+            },
+            coords={"time": [0.5, 1.5, 2.0], "shot": [0, 1]}
+        )
+    ),
+])
+def test_shift_time_to_not_nan(ds, how, subset, expected):
+    result = shift_time_to_not_nan(ds, episode_dim="shot", time_dim="time", how=how, subset=subset)
+    xr.testing.assert_identical(result, expected)
