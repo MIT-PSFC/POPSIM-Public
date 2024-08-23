185e99dd3a18a494c329267dc015b16a6cb3aef8
Allen Wang <allenw@mit.edu>
2024-08-23 13:52:17 -0400

    Use safer data load in module_eval_pipeline

--- popsim/tests/fixtures.py
+++ popsim/tests/fixtures.py
@@ -5,9 +5,12 @@ import popsim
 
 
 def load_cmod_test_dataset():
-    path_to_data = os.path.join(popsim.DATA_DIR, "cmod/saperstein_july_2024_subset.nc")
-    ds = xr.open_dataset(path_to_data)
-    return ds
+    try:
+        path_to_data = os.path.join(popsim.DATA_DIR, "cmod/saperstein_july_2024_subset.nc")
+        ds = xr.open_dataset(path_to_data)
+        return ds
+    except:
+        raise FileNotFoundError(f"Could not find the CMOD test dataset at {path_to_data}. Did you do a git lfs init followed by a git lfs pull?")
 
 @pytest.fixture
 def cmod_test_dataset():
