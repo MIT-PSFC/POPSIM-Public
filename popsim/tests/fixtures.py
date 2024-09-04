5a0c9c8e74fd119549fcd873b4ee07883d1c051d
Allen Wang <allenw@mit.edu>
2024-09-04 17:35:16 -0400

    Add a oscillator_dataset to fixtures

--- popsim/tests/fixtures.py
+++ popsim/tests/fixtures.py
@@ -2,7 +2,10 @@ import xarray as xr
 import os
 import pytest
 import popsim
-
+import jax
+import jax.numpy as jnp
+import diffrax
+from popsim.xarray_utils import solution_to_xarray
 
 def load_cmod_test_dataset():
     try:
@@ -18,6 +21,31 @@ def load_mast_thomson_test_dataset():
     ds = xr.open_dataset(path_to_data)
     return ds
 
+def generate_oscillator_dataset():
+    """Generate an oscillator dataset, following the example from: https://docs.kidger.site/diffrax/examples/neural_ode """
+    ts = jnp.linspace(0, 5, 50)
+    cases = 64
+
+    def generate_solution(key):
+        y0 = jax.random.uniform(key, (2,), minval=-0.6, maxval=1)
+        y0 = {"y0": y0[0], "y1": y0[1]}
+
+        def f(t, y, args):
+            y = jnp.array([y["y0"], y["y1"]])
+            x = y / (1 + y)
+            return {"y0": x[1], "y1": -x[0]}
+
+        solver = diffrax.Tsit5()
+        dt0 = 0.1
+        saveat = diffrax.SaveAt(ts=ts)
+        sol = diffrax.diffeqsolve(
+            diffrax.ODETerm(f), solver, ts[0], ts[-1], dt0, y0, saveat=saveat
+        )
+        return sol
+    keys = jax.random.split(jax.random.PRNGKey(0), cases)
+    sols = jax.vmap(lambda key: generate_solution(key))(keys)
+    return solution_to_xarray(sols, multi_simulation=True)
+
 @pytest.fixture
 def cmod_test_dataset():
     return load_cmod_test_dataset()
@@ -25,3 +53,7 @@ def cmod_test_dataset():
 @pytest.fixture(scope="session")
 def mast_thomson_test_dataset():
     return load_mast_thomson_test_dataset()
+
+@pytest.fixture
+def oscillator_dataset():
+    return generate_oscillator_dataset()
