8f672871687d37f6eeceffdb85965964d1b3a750
Allen Wang <allenw@mit.edu>
2024-09-04 17:34:55 -0400

    Define a PartitionFn and change the partition_pytree_by_members to instead be a make_partition_pytree_by_members

--- popsim/ml/partition.py
+++ popsim/ml/partition.py
@@ -1,3 +1,5 @@
+import typing
+
 import equinox as eqx
 from jaxtyping import PyTree
 
@@ -6,22 +8,42 @@ Partition functions and helpers.
 """
 
 
-def partition_pytree_by_members(pytree: PyTree, pytree_items: tuple[object]) -> tuple[PyTree, PyTree]:
-    """Given a PyTree and a tuple of items that are members of the PyTree, partition the PyTree into two PyTrees:
-    one containing the members of the PyTree that are in the tuple, and one containing the members of the PyTree that
-    are not in the tuple.
+# Define a function that partitions a PyTree into two PyTrees.
+PartitionFn = typing.Callable[[PyTree], tuple[PyTree, PyTree]]
+
+
+def partition_by_arraylike(pytree: PyTree) -> tuple[PyTree, PyTree]:
+    """Partition a PyTree into two PyTrees based on whether the elements are array-like.
 
     Args:
-        pytree (PyTree): the PyTree to partition.
-        pytree_items (tuple[object]): the items to partition the PyTree by.
+        pytree (PyTree): The PyTree to partition.
 
     Returns:
-        tuple[PyTree, PyTree]: the partitioned PyTrees. The first contains members that are in "pytree_items", and the
-        second contains members that are not in "pytree_items".
+        tuple[PyTree, PyTree]: A tuple of two PyTrees. The first PyTree contains the array-like elements of the input PyTree, and the second PyTree contains the non-array-like elements of the input PyTree.
     """
-    targ_ids = [id(t) for t in pytree_items]
 
-    def partition_spec(x):
-        return id(x) in targ_ids
+    return eqx.partition(pytree, eqx.is_array_like)
 
-    return eqx.partition(pytree, partition_spec)
+
+def make_partition_pytree_by_members(pytree_item_getter: typing.Callable[[PyTree], tuple[object]]) -> PartitionFn:
+    """Generate a partition function that partitions a PyTree into two PyTrees based on a set of members of the PyTree.
+    The set of members is specified by a "pytree_item_getter" function that takes a PyTree and returns a tuple of
+    objects that are members of the PyTree. The partition function will return two PyTrees: one containing the members
+    specified by the "pytree_item_getter" function and the other containing the remaining members of the PyTree.
+
+    Args:
+        pytree_item_getter (typing.Callable[[PyTree], tuple[object]]): A function that takes a PyTree and returns a tuple of objects that are members of the PyTree.
+
+    Returns:
+        PartitionFn: A function that partitions a PyTree into two PyTrees based on the set of members specified by the "pytree_item_getter" function.
+    """
+
+    def partition_fn(pytree: PyTree):
+        targ_ids = [id(t) for t in pytree_item_getter(pytree)]
+
+        def partition_spec(x):
+            return id(x) in targ_ids
+
+        return eqx.partition(pytree, partition_spec)
+
+    return partition_fn
--- popsim/tests/test_ml/test_partition.py
+++ popsim/tests/test_ml/test_partition.py
@@ -1,5 +1,5 @@
 import jax.numpy as jnp
-from popsim.ml.partition import partition_pytree_by_members
+from popsim.ml.partition import make_partition_pytree_by_members, partition_by_arraylike
 from jaxtyping import Array
 import chex
 
@@ -9,15 +9,45 @@ class DummyClass:
     y: dict[str, Array]
 
 
-def test_partition_pytree_by_members():
+def test_make_partition_pytree_by_members():
     # Test with a simple PyTree that contains a single array and a dictionary of arrays
     # We want a partition of tree0 where the first partition contains tree0.x and tree0.y["a"].
     tree0 = DummyClass(x=jnp.zeros(3), y={"a": jnp.arange(10), "b": jnp.ones(5)})
-    items_to_partition_by = (tree0.x, tree0.y["a"])
+
+
+    def items_to_partition_by(pytree):
+        return (pytree.x, pytree.y["a"])
 
     expected_tree0a = DummyClass(x=jnp.zeros(3), y={"a": jnp.arange(10), "b": None})
     expected_tree0b = DummyClass(x=None, y={"a": None, "b": jnp.ones(5)})
 
-    tree0a, tree0b = partition_pytree_by_members(tree0, items_to_partition_by)
+    partition_fn = make_partition_pytree_by_members(items_to_partition_by)
+
+    tree0a, tree0b = partition_fn(tree0)
     chex.assert_trees_all_equal(tree0a, expected_tree0a)
     chex.assert_trees_all_equal(tree0b, expected_tree0b)
+
+    # Now change the tree, and test that the partition function still works.
+    tree1 = DummyClass(x=jnp.ones(3), y={"a": jnp.arange(10), "b": jnp.ones(5)})
+    tree1a, tree1b = partition_fn(tree1)
+    expected_tree1a = DummyClass(x=jnp.ones(3), y={"a": jnp.arange(10), "b": None})
+    expected_tree1b = DummyClass(x=None, y={"a": None, "b": jnp.ones(5)})
+    chex.assert_trees_all_equal(tree1a, expected_tree1a)
+    chex.assert_trees_all_equal(tree1b, expected_tree1b)
+
+
+
+def test_partition_by_arraylike():
+    # Create a PyTree with array-like and non-array-like elements
+    tree = DummyClass(x=jnp.zeros(3), y={"a": jnp.arange(10), "b": "non-array"})
+
+    # Expected partitioned PyTrees
+    expected_arraylike_tree = DummyClass(x=jnp.zeros(3), y={"a": jnp.arange(10), "b": None})
+    expected_non_arraylike_tree = DummyClass(x=None, y={"a": None, "b": "non-array"})
+
+    # Partition the PyTree
+    arraylike_tree, non_arraylike_tree = partition_by_arraylike(tree)
+
+    # Assert that the partitioned PyTrees match the expected results
+    chex.assert_trees_all_equal(arraylike_tree, expected_arraylike_tree)
+    chex.assert_trees_all_equal(non_arraylike_tree, expected_non_arraylike_tree)
