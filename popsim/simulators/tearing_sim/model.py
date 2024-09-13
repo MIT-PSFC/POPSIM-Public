8e8ee4e9f029d2adadd2744ce93863f0d179369b
ZanderKeith <zkeith@mit.edu>
2024-09-13 15:59:19 -0400

    Added simple output to OMAS data structure for the TearingSim module.

--- popsim/simulators/tearing_sim/model.py
+++ popsim/simulators/tearing_sim/model.py
@@ -62,3 +62,39 @@ class TearingSim(ModuleBase):
 
         out = TearingSim.Output(lown_array_out=lown_array_out, b_field_poloidal_probes_out=b_field_poloidal_probes_out)
         return state_dot, out
+
+    def add_to_ods(self, ods, sim_xarray):
+        """Given an ods object and the simulation's xarray, fill the ods with the simulation's data.
+        This is entirely hard-coded for now. Should decide on a more general structure after we've done a few of these.
+        TODO(ZanderKeith) this breaks index ordering. For instance, if a probe was originally at index 100 in the device description
+        it might now be at index 5 or something. This is because you can't put an element in an ods array at index x + 1 if there's nothing at index x.
+        Since we're only simulating a subset of probes, the indices are all smaller.
+        This shouldn't be too much of a problem since the names of the probes are still there,
+        we just need to ensure that indices are not used as identifiers elsewhere.
+
+        Args:
+        ----
+        ods: ODS
+            The ods object which will simulation data added to it.
+        sim_xarray: xarray.Dataset
+            The output of the TearingSim after running the simulation.
+        """
+
+        # The only data implemented so far is from the BFieldPoloidalProbes module
+        probe_details = self.config.b_field_poloidal_probes_module.config.probe_details
+
+        for i, probe in enumerate(probe_details):
+            # Fields should already have a unit associated with it [T]
+            ods[f"magnetics.b_field_pol_probe[{i}].area"] = probe["area"]
+
+            ods["magnetics"]["b_field_pol_probe"][i]["field"]["data"] = sim_xarray[f"output.b_field_poloidal_probes_out.Bp.{probe['name']}"]
+            ods["magnetics"]["b_field_pol_probe"][i]["field"]["time"] = sim_xarray.time
+
+            ods["magnetics"]["b_field_pol_probe"][i]["identifier"] = probe["identifier"]
+            ods["magnetics"]["b_field_pol_probe"][i]["name"] = probe["name"]
+            ods["magnetics"]["b_field_pol_probe"][i]["position"]["r"] = probe["position"]["r"]
+            ods["magnetics"]["b_field_pol_probe"][i]["position"]["phi"] = probe["position"]["phi"]
+            ods["magnetics"]["b_field_pol_probe"][i]["position"]["z"] = probe["position"]["z"]
+
+            ods["magnetics"]["b_field_pol_probe"][i]["turns"] = 1  # Not in the device description?
+            ods["magnetics"]["b_field_pol_probe"][i]["type"]["index"] = probe["type"]["index"]
