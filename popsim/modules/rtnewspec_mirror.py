Fixed degree/rad conversion in rtnewspec_mirror, added option for separating n vs -n modes, added tests for rms calculation

popsim/modules/rtnewspec_mirror.py
@@ -1,19 +1,18 @@
 import dataclasses
 
 import chex
-import jax
 import jax.numpy as jnp
 
 from popsim import ModuleBase, discrete_time_field
-from popsim.modules.magnetic_diagnostics import BFieldPoloidalProbes
 
 FFT_SAMPLES = 2048
-SAMPLING_FREQUENCY = 60e3 # Hz
-REPORT_FREQUENCY = 1e3 # Hz
+SAMPLING_FREQUENCY = 60e3  # Hz
+REPORT_FREQUENCY = 1e3  # Hz
 
 """
 Mirror of rtnewspec, used to calculate n1rms (and n2rms, n3rms, etc.) in real-time on DIII-D.
 """
+
 
 @chex.dataclass
 class RTNewSpecMirror(ModuleBase):
@@ -35,6 +34,8 @@ class RTNewSpecMirror(ModuleBase):
         nsmth: int = dataclasses.field(default_factory=lambda: 3)
         # Maximum number of modes to look at.
         max_modes: int = dataclasses.field(default_factory=lambda: 3)
+        # Whether or not to treat n=-1,-2,etc. the same as n=1,2,etc.
+        separate_negative_n: bool = dataclasses.field(default_factory=lambda: False)
         # The frequency of updating the calculated RMS values [Hz]
         f_report: float = dataclasses.field(default_factory=lambda: REPORT_FREQUENCY)
 
@@ -43,13 +44,11 @@ class RTNewSpecMirror(ModuleBase):
         # The last nsamples of magnetic probe data
         probe1_data: jnp.ndarray = discrete_time_field(default=jnp.zeros(FFT_SAMPLES))
         probe2_data: jnp.ndarray = discrete_time_field(default=jnp.zeros(FFT_SAMPLES))
-        #previous_rms: float = discrete_time_field(default=0.0)
-        #time_until_next_report: float = discrete_time_field(default=0.0)
         uninitialized: bool = discrete_time_field(default=True)
 
     @chex.dataclass
     class Output:
-        rms: dict[int, float] # Mode number to RMS value
+        rms: dict[int, float]  # Mode number to RMS value
 
     @chex.dataclass
     class Params:
@@ -73,22 +72,10 @@ class RTNewSpecMirror(ModuleBase):
         # But that isn't really jax-like...
         # Maybe with jax.lax.cond?
 
-        # returned_rms = jax.lax.cond(
-        #     state.time_until_next_report <= 0.0,
-        #     lambda _: self.calculate_rms(probe1_data, probe2_data),
-        #     lambda _: state.previous_rms,
-        #     operand=None,
-        # )
         rms_values = self.calculate_rms(probe1_data, probe2_data)
 
-        state = RTNewSpecMirror.State(
-            probe1_data=probe1_data,
-            probe2_data=probe2_data,
-            uninitialized=False
-        )
-        output = RTNewSpecMirror.Output(
-            rms=rms_values
-        )
+        state = RTNewSpecMirror.State(probe1_data=probe1_data, probe2_data=probe2_data, uninitialized=False)
+        output = RTNewSpecMirror.Output(rms=rms_values)
 
         return state, output
 
@@ -101,38 +88,48 @@ class RTNewSpecMirror(ModuleBase):
         # 5. Filter cross spectrum to only have data where the coherence is above a threshold, and the phase matches an n-th mode
 
         # Take FFTs and only keep the positive frequencies
-        probe1_fft = jnp.fft.fft(probe1_data)[:self.config.nsamples//2]
-        probe2_fft = jnp.fft.fft(probe2_data)[:self.config.nsamples//2]
+        probe1_fft = jnp.fft.fft(probe1_data)[: self.config.nsamples // 2]
+        probe2_fft = jnp.fft.fft(probe2_data)[: self.config.nsamples // 2]
 
         # Calculate the auto spectrum of probe 1
+        # The FFT is B1(w), so the auto spectrum is B1(w)B1*(w) = P11(w)
         auto_spectrum = jnp.abs(probe1_fft) ** 2
 
-        # Smooth the auto spectrum using boxcar averaging. 
+        # Smooth the auto spectrum using boxcar averaging.
         boxcar = jnp.ones(self.config.nsmth) / self.config.nsmth
-        smoothed_auto_spectrum = jnp.convolve(auto_spectrum, boxcar, mode='same')
+        smoothed_auto_spectrum = jnp.convolve(auto_spectrum, boxcar, mode="same")
 
         # Calculate the cross spectrum of probe 1 and probe 2
         complex_cross_spectrum = probe1_fft * jnp.conj(probe2_fft)
         cross_phase = jnp.angle(complex_cross_spectrum)
         cross_phase_deg = jnp.round(jnp.rad2deg(cross_phase))
+        if not self.config.separate_negative_n:
+            cross_phase_deg = jnp.abs(cross_phase_deg)
 
         # Calculate coherence and set up 95% confidence interval
-        coherence = jnp.abs(complex_cross_spectrum) ** 2 / (auto_spectrum * jnp.abs(probe2_fft) ** 2)
-        c95 = 1.96/jnp.sqrt(2.0*self.config.nsmth - 2.0)
-        c95 = (jnp.exp(c95)-jnp.exp(-c95))/(jnp.exp(c95)+jnp.exp(-c95))
-        c95 = c95*c95
+        cross_power = jnp.abs(complex_cross_spectrum)
+        cross_coherence = cross_power / ((auto_spectrum * jnp.abs(probe2_fft) ** 2) ** 0.5)
+        c95 = 1.96 / jnp.sqrt(2.0 * self.config.nsmth - 2.0)
+        c95 = (jnp.exp(c95) - jnp.exp(-c95)) / (jnp.exp(c95) + jnp.exp(-c95))
+        c95 = c95 * c95
 
         rms_values = {}
 
         # For each mode, filter the auto spectrum based on coherence and phase
         for mode in range(1, self.config.max_modes + 1):
             filtered_spectrum = jnp.where(
-                (coherence > c95) & (cross_phase_deg == mode * jnp.round(jnp.deg2rad(self.config.d_theta))),
-                smoothed_auto_spectrum,
-                0.0
+                (cross_coherence > c95) & (cross_phase_deg == mode * jnp.round(self.config.d_theta)), smoothed_auto_spectrum, 0.0
             )
-            # Get the highest value in the filtered spectrum
-            rms = jnp.max(filtered_spectrum)*self.config.alpha
+            # Find the maximum value of the filtered spectrum and multiply by alpha to get the RMS value
+            rms = jnp.max(filtered_spectrum) * self.config.alpha
             rms_values[mode] = rms
+
+            # Repeat for negative modes if separate_negative_n is True
+            if self.config.separate_negative_n:
+                filtered_spectrum = jnp.where(
+                    (cross_coherence > c95) & (cross_phase_deg == -mode * jnp.round(self.config.d_theta)), smoothed_auto_spectrum, 0.0
+                )
+                rms = jnp.max(filtered_spectrum) * self.config.alpha
+                rms_values[-mode] = rms
 
         return rms_values

popsim/simulators/tearing_sim/model.py
@@ -1,9 +1,13 @@
 import chex
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+from jaxtyping import ArrayLike, PyTree
 
 from popsim import ModuleBase
 from popsim.modules.magnetic_diagnostics import BFieldPoloidalProbes, LowNArray
-from popsim.modules.tearing import Tearing
 from popsim.modules.rtnewspec_mirror import RTNewSpecMirror
+from popsim.modules.tearing import Tearing
 
 """
 Simulation for tearing modes and all the diagnostics which can measure them.
@@ -30,7 +34,7 @@ class TearingSim(ModuleBase):
     @chex.dataclass
     class Output:
         # Define the output variables that will be returned by the module.
-        # locals: PyTree[ArrayLike]
+        locals: PyTree[ArrayLike]
         lown_array_out: LowNArray.Output
         b_field_poloidal_probes_out: BFieldPoloidalProbes.Output
         rtnewspec_mirror_out: RTNewSpecMirror.Output
@@ -56,19 +60,25 @@ class TearingSim(ModuleBase):
         lown_array_out = self.config.lown_array_module(None, lown_array_params)
         b_field_poloidal_probes_out = self.config.b_field_poloidal_probes_module(None, b_field_poloidal_probes_params)
 
-        rtnewspec_params = RTNewSpecMirror.Params(probe1_signal=b_field_poloidal_probes_out.Bp[self.config.rtnewspec_mirror_module.config.probe1_id], 
-                                                  probe2_signal=b_field_poloidal_probes_out.Bp[self.config.rtnewspec_mirror_module.config.probe2_id])
+        rtnewspec_params = RTNewSpecMirror.Params(
+            probe1_signal=b_field_poloidal_probes_out.Bp[self.config.rtnewspec_mirror_module.config.probe1_id],
+            probe2_signal=b_field_poloidal_probes_out.Bp[self.config.rtnewspec_mirror_module.config.probe2_id],
+        )
         rtnewspec_mirror_state, rtnewspec_mirror_out = self.config.rtnewspec_mirror_module(state.rtnewspec_mirror_state, rtnewspec_params)
 
-        # # Filter out any non-array-like variables
-        # aux_data = eqx.filter(locals(), eqx.is_array_like)
-        # # Promote any scalar-like variables to arrays
-        # aux_data = jax.tree.map(jnp.asarray, aux_data)
+        # Filter out any non-array-like variables
+        aux_data = eqx.filter(locals(), eqx.is_array_like)
+        # Promote any scalar-like variables to arrays
+        aux_data = jax.tree.map(jnp.asarray, aux_data)
 
         # Make a state_dot.
-        state = TearingSim.State(tearing_state=tearing_state_dot,
-                                 rtnewspec_mirror_state=rtnewspec_mirror_state)
-        out = TearingSim.Output(lown_array_out=lown_array_out, b_field_poloidal_probes_out=b_field_poloidal_probes_out, rtnewspec_mirror_out=rtnewspec_mirror_out)
+        state = TearingSim.State(tearing_state=tearing_state_dot, rtnewspec_mirror_state=rtnewspec_mirror_state)
+        out = TearingSim.Output(
+            lown_array_out=lown_array_out,
+            b_field_poloidal_probes_out=b_field_poloidal_probes_out,
+            rtnewspec_mirror_out=rtnewspec_mirror_out,
+            locals=aux_data,
+        )
         return state, out
 
     def add_to_ods(self, ods, sim_xarray):

popsim/tests/test_modules/test_rtnewspec_mirror.py
@@ -12,7 +12,7 @@ def test_calculated_rms_simple():
         probe1_id="probe1",
         probe2_id="probe2",
         nsamples=2048,
-        alpha=1.0,
+        alpha=0.001, # TODO(ZanderKeith): This is some factor that needs to be included to get [T] out of the FFT.
         f_probe=60e3,
         nsmth=3,
         max_modes=2,
@@ -29,6 +29,77 @@ def test_calculated_rms_simple():
     rms_values = rtnewspec_mirror.calculate_rms(probe1_signal, probe2_signal)
 
     # Check that the output is large for n=1 but small for n=2
-    assert rms_values[1] > 0.9
+    assert rms_values[1] > 1.0
     assert rms_values[2] < 0.1
+
+def test_calculated_rms_inverted_phase():
+    """Test the RMS calculation of a simple sinusoidal signal with known phase offset, but inverted."""
+
+    probe_offset = jnp.pi/5
+
+    config = RTNewSpecMirror.Config(
+        d_theta=jnp.rad2deg(probe_offset),
+        probe1_id="probe1",
+        probe2_id="probe2",
+        nsamples=2048,
+        alpha=0.001, # TODO(ZanderKeith): This is some factor that needs to be included to get [T] out of the FFT.
+        f_probe=60e3,
+        nsmth=3,
+        max_modes=2,
+    )
+
+    rtnewspec_mirror = RTNewSpecMirror(config=config)
+
+    # Make repeating sinusoidal signals with 10 kHz frequency as if they were being sampled at 60 kHz
+    time = jnp.arange(2048)/60e3
+    probe1_signal = jnp.sin(2*jnp.pi*10e3*time)
+    probe2_signal = jnp.sin(2*jnp.pi*10e3*time + probe_offset)
+
+    # Calculate the RMS values directly
+    rms_values = rtnewspec_mirror.calculate_rms(probe1_signal, probe2_signal)
+
+    # Check that the output is large for n=1 but small for n=2
+    assert rms_values[1] > 1.0
+    assert rms_values[2] < 0.1
+
+def test_calculated_rms_separate_negative_n():
+    """Test the reconstruction of a sinusoidal signal with two modes, one positive and one negative."""
+
+    probe_offset = jnp.pi/5
+
+    config = RTNewSpecMirror.Config(
+        d_theta=jnp.rad2deg(probe_offset),
+        probe1_id="probe1",
+        probe2_id="probe2",
+        nsamples=2048,
+        alpha=0.001, # TODO(ZanderKeith): This is some factor that needs to be included to get [T] out of the FFT.
+        f_probe=60e3,
+        nsmth=3,
+        max_modes=2,
+        separate_negative_n=True,
+    )
+
+    rtnewspec_mirror = RTNewSpecMirror(config=config)
+
+    # Make repeating sinusoidal signals with 10 kHz frequency as if they were being sampled at 60 kHz
+    # Here we have an n=1 mode and a n=-2 mode
+    time = jnp.arange(2048)/60e3
+    probe1_n1_signal = 0.5*jnp.sin(2*jnp.pi*10e3*time)
+    probe2_n1_signal = 0.5*jnp.sin(2*jnp.pi*10e3*time - probe_offset)
+    probe1_n2_signal = 0.5*jnp.sin(2*(2*jnp.pi*10e3*time))
+    probe2_n2_signal = 0.5*jnp.sin(2*(2*jnp.pi*10e3*time + probe_offset))
+
+    probe1_signal = probe1_n1_signal + probe1_n2_signal
+    probe2_signal = probe2_n1_signal + probe2_n2_signal
+
+    # Calculate the RMS values directly
+    rms_values = rtnewspec_mirror.calculate_rms(probe1_signal, probe2_signal)
+
+    # Check that the output is large for n=1 and n=-2
+    # but small for n=2 and n=-1
+    assert rms_values[1] > 1.0
+    assert rms_values[-2] > 1.0
     
+    assert rms_values[-1] < 0.1
+    assert rms_values[2] < 0.1
+