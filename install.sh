a94d4bdc79561f5261350d6a824948f181af9dba
awang <awang@ip-10-13-78-57.us-east-2.compute.internal>
2024-10-02 22:17:36 +0000

    Break out a separate build_radash.sh

--- install.sh
+++ install.sh
@@ -67,19 +67,7 @@ if [[ "$install_dev" =~ ^[yY] ]]; then
     pre-commit install # Install pre-commit hooks
 fi
 
-# Run radas except in CI environment.
-if [ -z "$CI" ]; then
-    echo "Retrieving ATOMIC_DATA_PATH from popsim module..."
-    ATOMIC_DATA_PATH=$(poetry run python -c "from popsim import ATOMIC_DATA_PATH; print(ATOMIC_DATA_PATH)")
-    
-    # Run the radas command using the obtained path within the Poetry environment
-    echo "Running radas. This might take a while (sorry)!"
-    poetry run radas -d "$ATOMIC_DATA_PATH" --verbose
-    echo "Moving the radas output files to $ATOMIC_DATA_PATH..."
-    mv ${ATOMIC_DATA_PATH}/output/*.nc "${ATOMIC_DATA_PATH}/"
-else
-    echo "Skipping radas execution in CI environment."
-fi
+./build_radas.sh
 
 # Check for git lfs and install if not present
 echo "Checking for Git LFS..."
