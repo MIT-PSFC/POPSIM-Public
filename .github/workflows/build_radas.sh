a2d9c726ecbbf16db06a80bed5cc89e2dbe6c430
Allen Wang <allenw@mit.edu>
2024-10-02 22:57:25 -0400

    Remove radas as dep, just do a pip install in build_radas.sh

--- .github/workflows/build_radas.sh
+++ .github/workflows/build_radas.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-
 ATOMIC_DATA_PATH=$(git rev-parse --show-toplevel)/popsim/atomic_data
 
 echo "Running radas. This might take a while (sorry)!"
-poetry run radas -d "$ATOMIC_DATA_PATH" --verbose
+
+python3 -m pip install radas==2024.8.0
+
+python3 -m radas -d "$ATOMIC_DATA_PATH" --verbose
 
 echo "Moving the radas output files to $ATOMIC_DATA_PATH..."
 mv ${ATOMIC_DATA_PATH}/output/*.nc "${ATOMIC_DATA_PATH}/"
+
+# TODO(allenw): remove this once cfspopcon is no longer a submodule
 cp ${ATOMIC_DATA_PATH}/*.nc ./submodules/cfspopcon/cfspopcon/atomic_data/
