a75308e85ae9d5c298208db04eea67cff778ecb7
Allen Wang <allenw@mit.edu>
2024-10-02 22:12:34 -0400

    Add back the movement of atomic data to cfspopcon

--- .github/workflows/build_radas.sh
+++ .github/workflows/build_radas.sh
@@ -8,3 +8,4 @@ poetry run radas -d "$ATOMIC_DATA_PATH" --verbose
 
 echo "Moving the radas output files to $ATOMIC_DATA_PATH..."
 mv ${ATOMIC_DATA_PATH}/output/*.nc "${ATOMIC_DATA_PATH}/"
+cp ${ATOMIC_DATA_PATH}/*.nc ./submodules/cfspopcon/cfspopcon/atomic_data/
