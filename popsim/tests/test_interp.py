10adf6d912a234e5c69a5698e8f6b34b476197a6
Allen Wang <allenw@mit.edu>
2024-10-16 10:13:00 -0400

    Add test for interp_over_nans

--- popsim/tests/test_interp.py
+++ popsim/tests/test_interp.py
@@ -68,3 +68,60 @@ def test_linear_interp_time_dic(interp_type):
         chex.assert_trees_all_close(interp.resolve_paths(interpolated_tree, 1.0), expected2)
     elif interp_type == interp.InterpType.RECTILINEAR:
         chex.assert_trees_all_close(interp.resolve_paths(interpolated_tree, 1.0 + np.finfo(np.float32).eps), expected2)
+
+@pytest.mark.parametrize(
+    "ts, data_input, expected_output",
+    [
+        # Test case 1: Simple array with NaNs in data
+        (
+            np.array([0, 1, 2, 3, 4, 5]),
+            np.array([1.0, np.nan, 3.0, np.nan, 5.0, 6.0]),
+            np.array([1.0, 1.0, 1.0, 3.0, 3.0, 5.0]),
+        ),
+        # Test case 2: Data is a dictionary of arrays with NaNs
+        (
+            np.array([0, 1, 2, 3, 4]),
+            {
+                'a': np.array([1.0, 1.0, np.nan, 3.0, 4.0]),
+                'b': np.array([0.0, np.nan, 2.0, np.nan, 4.0]),
+            },
+            {
+                'a': np.array([1.0, 1.0, 1.0, 1.0, 3.0]),
+                'b': np.array([0.0, 0.0, 0.0, 2.0, 2.0]),
+            },
+        ),
+        # Test case 3: Data is a list of arrays with NaNs
+        (
+            np.array([0, 1, 2, 3]),
+            [
+                np.array([1.0, 1.0, np.nan, 3.0]),
+                np.array([0.0, np.nan, 2.0, np.nan]),
+            ],
+            [
+                np.array([1.0, 1.0, 1.0, 1.0]),
+                np.array([0.0, 0.0, 0.0, 2.0]),
+            ],
+        ),
+        # Test case 4: Data with NaNs at the beginning (doesn't handle this case)
+        (
+            np.array([0, 1, 2, 3, 4]),
+            np.array([np.nan, np.nan, 2.0, 3.0, 3.0]),
+            np.array([np.nan, np.nan, np.nan, 2.0, 3.0]),
+        ),
+        # Test case 5: Data with NaNs at the end
+        (
+            np.array([0, 1, 2, 3, 4]),
+            np.array([0.0, 1.0, 2.0, np.nan, np.nan]),
+            np.array([0.0, 0.0, 1.0, 2.0, 2.0]),
+        ),
+        # Test case 6: Data with no NaNs
+        (
+            np.array([0, 1, 2, 3]),
+            np.array([0.0, 1.0, 2.0, 2.0]),
+            np.array([0.0, 0.0, 1.0, 2.0]),
+        ),
+    ],
+)
+def test_interp_over_nans(ts, data_input, expected_output):
+    result = interp.interp_over_nans(ts, data_input)
+    chex.assert_trees_all_close(result, expected_output)
