407ffff69b4bba241eb7af20283403b182562896
Zander Keith <zkeith@mit.edu>
2024-11-21 14:03:38 -0500

    Removed unnecessary code from tearing_sim/model.py, updated tearing_sim notebook to have reduced prove frequency to run faster for example purposes.

--- popsim/simulators/tearing_sim/model.py
+++ popsim/simulators/tearing_sim/model.py
@@ -1,6 +1,6 @@
 import chex
 
-from popsim import ModuleBase, discrete_no_save_field
+from popsim import ModuleBase
 from popsim.modules.magnetic_diagnostics import BFieldPoloidalProbes, LowNArray
 from popsim.modules.rtnewspec_mirror import RTNewSpecMirror
 from popsim.modules.tearing import Tearing
@@ -25,12 +25,11 @@ class TearingSim(ModuleBase):
         # Define the differential state variables that will be integrated during the simulation.
         # If a variable is defined in here, then the module must output its time derivative in the __call__ method.
         tearing_state: Tearing.State
-        rtnewspec_mirror_state: RTNewSpecMirror.State = discrete_no_save_field()
+        rtnewspec_mirror_state: RTNewSpecMirror.State
 
     @chex.dataclass
     class Output:
         # Define the output variables that will be returned by the module.
-        # locals: PyTree[ArrayLike]
         lown_array_out: LowNArray.Output
         b_field_poloidal_probes_out: BFieldPoloidalProbes.Output
         rtnewspec_mirror_out: RTNewSpecMirror.Output
@@ -62,18 +61,12 @@ class TearingSim(ModuleBase):
         )
         rtnewspec_mirror_state, rtnewspec_mirror_out = self.config.rtnewspec_mirror_module(state.rtnewspec_mirror_state, rtnewspec_params)
 
-        # Filter out any non-array-like variables
-        # aux_data = eqx.filter(locals(), eqx.is_array_like)
-        # Promote any scalar-like variables to arrays
-        # aux_data = jax.tree.map(jnp.asarray, aux_data)
-
         # Make a state_dot.
         state = TearingSim.State(tearing_state=tearing_state_dot, rtnewspec_mirror_state=rtnewspec_mirror_state)
         out = TearingSim.Output(
             lown_array_out=lown_array_out,
             b_field_poloidal_probes_out=b_field_poloidal_probes_out,
             rtnewspec_mirror_out=rtnewspec_mirror_out,
-            # locals=aux_data,
         )
         return state, out
 
