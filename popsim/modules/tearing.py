d8ef209a001fffc92ce64ca05b5bb07392e03671
Zander Keith <zkeith@mit.edu>
2025-01-16 16:15:48 -0500

    Added flag to EFLM module inputs to say if there exists a rational surface or not.

--- popsim/modules/tearing.py
+++ popsim/modules/tearing.py
@@ -467,18 +467,23 @@ def calculate_locking_threshold(scaling_law_params, scaling_law_terms):
 
 
 def locked_mode_dynamics(
-    config: "ErrorFieldLocking.Config", state: "ErrorFieldLocking.State", overlap: float, locking_threshold: float
+    config: "ErrorFieldLocking.Config",
+    state: "ErrorFieldLocking.State",
+    overlap: float,
+    locking_threshold: float,
+    rational_surface_exists: bool,
 ) -> TearingPhase:
     """Determine how the locked mode evolves based on the overlap and the locking threshold.
 
     If there is no mode, and the overlap exceeds the locking threshold, then a locked mode will form.
     If there is a locked mode, and the overlap drops below the hystereis fraction of the locking threshold, then the mode will go away.
+    If there is no rational surface, then the mode is forced unlocked.
 
     Args:
         config (ErrorFieldLocking.Config): The configuration for the module.
         state (ErrorFieldLocking.State): The state of the module.
-        overlap (float): The overlap of the error field.
-        locking_threshold (float): The threshold for locking the mode.
+        overlap (float): The present overlap of the error field.
+        locking_threshold (float): The presently calculated empirical threshold of a locked mode forming
 
     Returns:
         TearingPhase: The new phase of the mode
@@ -487,6 +492,7 @@ def locked_mode_dynamics(
     # TODO(zkeith): In the future when we may need to do more than just locked and unlocked, we should make this more sophisticated.
     # Might get a little screwy since select_w_tuples only works with one condition at a time.
     threshold_check = [
+        (rational_surface_exists == 0, TearingPhase.NONE),
         (overlap > locking_threshold, TearingPhase.LOCKED),
         (overlap < locking_threshold * config.hysteresis, TearingPhase.NONE),
     ]
@@ -534,6 +540,7 @@ class ErrorFieldLocking(ModuleBase):
         scaling_law_params: dict[str, float]  # Values for each parameter in the scaling law
         pf_active_circuit_current: dict[str, float]  # Current in PF coils over time
         overlaps: dict[str, dict[str, complex]]  # Overlaps for each coil source
+        rational_surface_exists: int  # Placeholder. Must have some term (be it q90 or something) that indicates the existence of a rational surface
         cur_per_W: float = 1e3 / 1e-2  # Perturbed current per island width [A/m] TODO(ZanderKeith) a guess for now
 
     @chex.dataclass
@@ -552,10 +559,6 @@ class ErrorFieldLocking(ModuleBase):
 
     def __init__(self, config):
         self.config = config
-
-        # TODO(zkeith): verify with Matt that this calculation is no longer necessary with new data file
-        # Also ask what current was in the TF's for this data file, and how much is expected to change between 12T and 8T operation phase = np.exp(1j * 2 * np.pi * i / n_tf + config.tf_phase_offset)
-
         self.delta_static: complex = np.sum([self.config.overlaps[key]["nominal"] for key in self.config.static_sources])
 
     def __call__(
@@ -569,9 +572,8 @@ class ErrorFieldLocking(ModuleBase):
         locking_threshold = calculate_locking_threshold(params["scaling_law_params"], params["scaling_law_terms"])
 
         # If the overlap is greater than the threshold, the mode is locked
-        new_tearing_phase = locked_mode_dynamics(self.config, state, error_field_overlap, locking_threshold)
+        new_tearing_phase = locked_mode_dynamics(self.config, state, error_field_overlap, locking_threshold, params.rational_surface_exists)
 
-        # TODO (zkeith): Right now we're only doing the 2/1 mode, I'm unsure if we need to account for all modes in this.
         for mode in self.config.modes:
             state.W[mode] = jnp.where(new_tearing_phase == TearingPhase.LOCKED, 1e-2, 0)
 
