8c8c5ea62df029cbc8a3734bc0a0dcc2398b6e74
Allen Wang <allenw@mit.edu>
2025-07-24 16:43:05 -0400

    Spot checks in test_export that reload the JSON and compare to model

--- popsim/tests/test_ml/test_export.py
+++ popsim/tests/test_ml/test_export.py
@@ -2,6 +2,8 @@ from popsim.tests.fixtures import profile_predictor_latest_sparc
 from popsim.ml._export import export
 import tempfile
 from pathlib import Path
+import numpy as np
+import json
 
 def test_export(profile_predictor_latest_sparc):
     trainer, _, _, test_dl = profile_predictor_latest_sparc
@@ -10,4 +12,18 @@ def test_export(profile_predictor_latest_sparc):
         tmpdir = Path(tmpdir)
         export(trainer.train_state.model, tmpdir, test_dl)
         assert (tmpdir / "model.json").exists()
-        assert (tmpdir / "validation_data.nc").exists()
+        assert (tmpdir / "eval_data.nc").exists()
+    
+        # Load the exported model in JSON format and spot-check some matrices.
+        with open(tmpdir / "model.json", "r") as f:
+            model_json = json.load(f)
+            
+            n_layers = len(model_json['model_parameters']['profile_predictor']['nn']['layers'])
+            for i in range(n_layers):
+                wj = np.asarray(model_json['model_parameters']['profile_predictor']['nn']['layers'][i]['weight'])
+                wm = trainer.train_state.model.profile_predictor.nn.layers[i].weight
+                assert np.allclose(wj, wm), "First layer weights do not match!"
+                
+                bj = np.asarray(model_json['model_parameters']['profile_predictor']['nn']['layers'][i]['bias'])
+                bm = trainer.train_state.model.profile_predictor.nn.layers[i].bias
+                assert np.allclose(bj, bm), "First layer biases do not match!"
