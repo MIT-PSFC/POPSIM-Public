145d8d7303063803f8069f8a8628426eec3dffd4
Allen Wang <allenw@mit.edu>
2025-08-21 15:10:17 -0400

    Add test_mat_to_xr_cli

--- popsim/tests/test_interfaces/test_meq2xarray.py
+++ popsim/tests/test_interfaces/test_meq2xarray.py
@@ -1,14 +1,15 @@
 import pytest
-from popsim.interfaces.meq2xarray import tcv_db_to_xr, loadmat, db_mat_to_xr_cli
+from popsim.interfaces.meq2xarray import tcv_db_to_xr, loadmat, mat_to_xr_cli
 import os
 from popsim import DATA_DIR
 import xarray as xr
 import tempfile
+from click.testing import CliRunner
 
 
 @pytest.fixture
 def file_path():
-    return os.path.join(DATA_DIR, "TCV_meqdb_template.mat")
+    return os.path.join(DATA_DIR, "tcv/TCV_meqdb_template.mat")
 
 
 def test_tcv_db_to_xr(file_path):
@@ -20,3 +21,19 @@ def test_tcv_db_to_xr(file_path):
 def test_loadmat(file_path):
     d = loadmat(file_path)
     assert isinstance(d, dict)
+
+
+def test_mat_to_xr_cli(file_path):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        runner = CliRunner()
+        result = runner.invoke(mat_to_xr_cli, [
+            file_path,
+            temp_dir,
+            "db",
+        ])
+        
+        assert result.exit_code == 0, f"CLI failed with error: {result.output}"
+
+        file_out = os.path.join(temp_dir, "TCV_meqdb_template.nc")
+        dt = xr.open_datatree(file_out)
+        assert isinstance(dt, xr.DataTree)
